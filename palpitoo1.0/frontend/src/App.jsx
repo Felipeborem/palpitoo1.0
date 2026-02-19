da88e5e685dacc1762a1b040b97fd51cdd3faf01
Felipe <felipeborem@gmail.com>
2026-02-19 13:38:09 -0300

    alteração algumas paginas para mandar para o rodrigo dia 19-2 by felipe

---
+++ palpitoo1.0/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Palpitoo</h1>
+        <p>Welcome to the betting app!</p>
+      </div>
+    </>
+  )
+}
+
+export default App
